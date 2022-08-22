bb910878f4913cafaa33facb14124d0998a53b61
ukm202 <ukm202@nyu.edu>
2022-08-22 19:35:08 +0545

    Responsive header created

--- imperial-engineering-associates/src/components/layout/Header/Header.js
+++ imperial-engineering-associates/src/components/layout/Header/Header.js
@@ -1,8 +1,39 @@
 import "./Header.css";
+import { FaFacebookF,FaTwitter, FaInstagram, FaLinkedinIn } from "react-icons/fa";
+import { MdLocalPhone,MdOutlineMail } from "react-icons/md";
+import Logo from "./Logo";
+
 const Header = () => {
     return (  
-        <header>
-            Header
+        <header className="header">
+            <Logo />
+            <div className="content">
+                    <div className="header-contact">
+                        <div>
+                            <a href="tel:123-456-7890" > <MdLocalPhone fill="#860c1e" /> +977 123-456-7890</a>
+
+                        </div>
+                        <div>
+                            <a href = "mailto: abc@example.com"><MdOutlineMail fill="#860c1e" /> abc@example.com</a>
+
+
+                        </div>
+                    </div>
+                        
+                    <div className="icons">
+
+                        <a href="#" ><FaFacebookF className="spin-animation" /></a>
+                        <a href="#"><FaTwitter  className="spin-animation" /></a>
+                        <a href="#"><FaInstagram className="spin-animation"  /></a>
+                        <a href="#"><FaLinkedinIn className="spin-animation"  /></a>
+                    </div>
+            </div>
+
+                
+
+            
+            
+            
         </header>
     );
 }
---
+++ imperial-engineering-associates/src/components/layout/Header/Logo.js
@@ -0,0 +1,14 @@
+import LogoImage from "../../../Images/logo.png";
+
+const Logo = () => {
+    return (  
+        <div className="logo">
+                    <a href="#">
+                        <img src={LogoImage} alt="Logo" />
+                        <p>Imperial Engineering Associates</p>
+                    </a>  
+        </div>
+    );
+}
+ 
+export default Logo;

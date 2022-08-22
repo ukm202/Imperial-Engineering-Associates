2cd6532918f3b72305a206440b5ca043524c3e82
ukm202 <ukm202@nyu.edu>
2022-08-22 19:37:39 +0545

    Code formatted

--- imperial-engineering-associates/src/components/layout/Header/Header.js
+++ imperial-engineering-associates/src/components/layout/Header/Header.js
@@ -1,41 +1,49 @@
 import "./Header.css";
-import { FaFacebookF,FaTwitter, FaInstagram, FaLinkedinIn } from "react-icons/fa";
-import { MdLocalPhone,MdOutlineMail } from "react-icons/md";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedinIn,
+} from "react-icons/fa";
+import { MdLocalPhone, MdOutlineMail } from "react-icons/md";
 import Logo from "./Logo";
 
 const Header = () => {
-    return (  
-        <header className="header">
-            <Logo />
-            <div className="content">
-                    <div className="header-contact">
-                        <div>
-                            <a href="tel:123-456-7890" > <MdLocalPhone fill="#860c1e" /> +977 123-456-7890</a>
+  return (
+    <header className="header">
+      <Logo />
+      <div className="content">
+        <div className="header-contact">
+          <div>
+            <a href="tel:123-456-7890">
+              {" "}
+              <MdLocalPhone fill="#860c1e" /> +977 123-456-7890
+            </a>
+          </div>
+          <div>
+            <a href="mailto: abc@example.com">
+              <MdOutlineMail fill="#860c1e" /> abc@example.com
+            </a>
+          </div>
+        </div>
 
-                        </div>
-                        <div>
-                            <a href = "mailto: abc@example.com"><MdOutlineMail fill="#860c1e" /> abc@example.com</a>
+        <div className="icons">
+          <a href="#">
+            <FaFacebookF className="spin-animation" />
+          </a>
+          <a href="#">
+            <FaTwitter className="spin-animation" />
+          </a>
+          <a href="#">
+            <FaInstagram className="spin-animation" />
+          </a>
+          <a href="#">
+            <FaLinkedinIn className="spin-animation" />
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-
-                        </div>
-                    </div>
-                        
-                    <div className="icons">
-
-                        <a href="#" ><FaFacebookF className="spin-animation" /></a>
-                        <a href="#"><FaTwitter  className="spin-animation" /></a>
-                        <a href="#"><FaInstagram className="spin-animation"  /></a>
-                        <a href="#"><FaLinkedinIn className="spin-animation"  /></a>
-                    </div>
-            </div>
-
-                
-
-            
-            
-            
-        </header>
-    );
-}
- 
 export default Header;
